098cfc36920a894247d0b54a7ff32b22d3e091b1
Kazuki_Yasumoto <Kazuki_Yasumoto@Kazuki-Yasumoto-no-MacBook-Air.local>
2016-11-07 23:31:33 +0900

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,33 @@
 class UsersController < ApplicationController
 
+  before_action :user_params, only:[:edit, :update, :create]
+
 
   def show
   end
 
   def edit
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @gender = user.gender
-    @grade = user.grade
     @studies = user.studies.page(params[:page]).per(5).order("created_at DESC")
     @courses = user.courses.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def update
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @gender = user.gender
-    @grade = user.grade
     @studies = user.studies.page(params[:page]).per(5).order("created_at DESC")
     @courses = user.courses.page(params[:page]).per(5).order("created_at DESC")
   end
 
 
   def create
+    @studies = user.studies.page(params[:page]).per(5).order("created_at DESC")
+    @courses = user.courses.page(params[:page]).per(5).order("created_at DESC")
+    @genders = user.genders.page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  private
+  def user_params
     user = User.find(params[:id])
     @nickname = user.nickname
     @gender = user.gender
     @grade = user.grade
-    @studies = user.studies.page(params[:page]).per(5).order("created_at DESC")
-    @courses = user.courses.page(params[:page]).per(5).order("created_at DESC")
-    @genders = user.genders.page(params[:page]).per(5).order("created_at DESC")
   end
 end
